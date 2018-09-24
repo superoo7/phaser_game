65f252e67b662355af9cea204bc540a0a5030a0b
Lai Weng Han <jlwhoo7@gmail.com>
2018-09-24 18:42:52 +0800

    Init

---
+++ src/global.d.ts
@@ -0,0 +1,14 @@
+declare module "assets/*" {
+  var _a: any;
+  export = _a;
+}
+
+declare module "*.png" {
+  var _a: any;
+  export = _a;
+}
+
+declare module "*.json" {
+  var _a: any;
+  export = _a;
+}
